3258f67b2a9830a4152426e5871000f5a455b190
Jason Fleagle <jasonfleagle@hotmail.com>
2016-01-07 02:54:31 +0000

    Added new challenges

---
+++ Day0_Hello_World.rb
@@ -0,0 +1,3 @@
+# Hello World application.
+print "Hello World.
+Welcome to the 30 Days of Code Challenge."
---
+++ Day4/Logical_Operators.js
@@ -0,0 +1,10 @@
+function Person(initial_Age){
+    // Add some more code to run some checks on initial_Age
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+   };
+}
