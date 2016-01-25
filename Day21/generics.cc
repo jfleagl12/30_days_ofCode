62bd57e3c980015d2e64cf548fa38ed419cf6f47
Jason Fleagle <jasonfleagle@hotmail.com>
2016-01-25 13:21:39 +0000

    Added day21 info

---
+++ Day21/generics.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Add your code here
+
+
+int main()
+{
+    vector<int> vInt{1, 2, 3};
+    vector<string> vString{"Hello", "World"};
+    
+    printArray<int>(vInt);
+    printArray<string>(vString);
+    
+    return 0;
+}
